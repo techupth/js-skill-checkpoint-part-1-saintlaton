2978f1fcc1e0983977f78b181e332deca20e0734
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-05-22 15:16:30 +0700

    feat:done all checkpoints

--- question-3.js
+++ question-3.js
@@ -1,3 +1,14 @@
 // Question #3
-let userPassword = "";
+let userPassword = "aaaaaaaaaaa";
 // เริ่มเขียนโค้ดตรงนี้
+function checkPasswordStrength(userPassword) {
+  if (userPassword.length < 6) {
+    return "Weak";
+  } else if (userPassword.length >= 6 && userPassword.length <= 10) {
+    return "Medium";
+  } else if (userPassword.length > 10) {
+    return "Strong";
+  }
+}
+
+console.log(checkPasswordStrength(userPassword));
--- question-5.js
+++ question-5.js
@@ -17,5 +17,27 @@ const products = [
   },
 ];
 
-const promotionCode = "";
+const promotionCode = "SALE50";
 // เริ่มเขียนโค้ดตรงนี้
+
+let totalPrice = 0;
+function calculateTotalPrice(products, promotionCode) {
+  for (let i = 0; i < products.length; i++) {
+    if (promotionCode === "SALE20") {
+      totalPrice =
+        totalPrice +
+        (products[i]["price"] - products[i]["price"] * 0.2) *
+          products[i]["quantity"];
+    } else if (promotionCode === "SALE50") {
+      totalPrice =
+        totalPrice +
+        (products[i]["price"] - products[i]["price"] * 0.5) *
+          products[i]["quantity"];
+    } else {
+      totalPrice = totalPrice + products[i]["price"] * products[i]["quantity"];
+    }
+  }
+  return totalPrice;
+}
+
+console.log(calculateTotalPrice(products, promotionCode));
